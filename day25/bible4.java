29247da3ecb06b73848fd5e0ee87af43c30de337
yunjunghun0116 <yunjunghun0116@gmail.com>
2023-12-05 16:27:07 +0900

    day26 - lv2문제 풀이

--- day25/bible4.java
+++ day25/bible4.java
@@ -9,6 +9,6 @@ public class bible4 {
         if (isAdd) {
             return common[common.length - 1] + common[1] - common[0];
         }
-        return return common[common.length - 1] * (common[1] / common[0]);
+        return common[common.length - 1] * (common[1] / common[0]);
     }
 }
