9dfbf0c926b3ef27633d28874f9e26ea899068f5
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-04-11 23:29:38 +0900

    [level 3] Title: 가장 먼 노드, Time: 1117.08 ms, Memory: 390 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -0,0 +1,67 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] edge) {
+        int[] answer = new int[n+1];
+        Arrays.fill(answer,Integer.MAX_VALUE);
+
+        Stack<int[]> destStack = new Stack<>();
+        Map<Integer,List<Integer>> edgeMap = new HashMap<>();
+
+        for(int[] ed : edge){
+            List<Integer> destList;
+            if(edgeMap.containsKey(ed[0])){
+                destList = edgeMap.get(ed[0]);
+            }else{
+                destList = new ArrayList<>();
+            }
+            destList.add(ed[1]);
+            edgeMap.put(ed[0],destList);
+
+            List<Integer> desList;
+            if(edgeMap.containsKey(ed[1])){
+                desList = edgeMap.get(ed[1]);
+            }else{
+                desList = new ArrayList<>();
+            }
+            desList.add(ed[0]);
+            edgeMap.put(ed[1],desList);
+        }
+
+        answer[0] = 1;
+        answer[1] = 1;
+        // node / dist
+        destStack.push(new int[]{1,1});
+
+        while(!destStack.isEmpty()){
+            // node / dist
+            int[] popNode = destStack.pop();
+            List<Integer> destList = edgeMap.get(popNode[0]);
+            for(int dest : destList){
+                int dist = popNode[1]+1;
+                if(dist < answer[dest]){
+                    answer[dest] = dist;
+                    destStack.push(new int[]{dest,dist});
+                }
+            }
+        }
+        
+        
+
+        int maxValue = Integer.MIN_VALUE;
+        int maxCount = 0;
+        for(int i = 0; i <= n;i++){
+            System.out.print(answer[i]+" ");
+            if(answer[i] > maxValue){
+                maxValue = answer[i];
+                maxCount = 1;
+                continue;
+            }
+            if(answer[i] == maxValue){
+                maxCount++;
+            }
+        }
+
+        return maxCount;
+    }
+}
