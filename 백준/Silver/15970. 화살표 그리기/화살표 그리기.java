09a00938314223b461c4d5bf48d28ef16a8af986
YunJungHun <76200940+yunjunghun0116@users.noreply.github.com>
2024-08-29 01:19:17 +0900

    [Silver IV] Title: 화살표 그리기, Time: 372 ms, Memory: 28224 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/15970. 화살표 그리기/화살표 그리기.java
+++ 백준/Silver/15970. 화살표 그리기/화살표 그리기.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     // 20291 15970
-    static List<Integer> whitePoints = new ArrayList<>();
-    static List<Integer> blackPoints = new ArrayList<>();
+    static Map<Integer, List<Integer>> dotsMap = new HashMap<>();
     static int n;
 
     static void input() {
@@ -15,44 +17,35 @@ public class Main {
             var input = sc.nextLine().split(" ");
             int num = Integer.parseInt(input[0]);
             int color = Integer.parseInt(input[1]);
-            if (color == 1) {
-                whitePoints.add(num);
-                continue;
-            }
-            blackPoints.add(num);
+            var currDots = dotsMap.getOrDefault(color, new ArrayList<>());
+            currDots.add(num);
+            dotsMap.put(color, currDots);
         }
     }
 
     static void solution() {
-        whitePoints.sort((a, b) -> a - b);
-        blackPoints.sort((a, b) -> a - b);
         int sum = 0;
-
-        for (int i = 0; i < whitePoints.size(); i++) {
-            if (i == 0) {
-                sum += whitePoints.get(1) - whitePoints.get(0);
-                continue;
-            }
-            if (i == whitePoints.size() - 1) {
-                sum += whitePoints.get(whitePoints.size() - 1) - whitePoints.get(whitePoints.size() - 2);
-                continue;
-            }
-            sum += Math.min(Math.abs(whitePoints.get(i - 1) - whitePoints.get(i)), Math.abs(whitePoints.get(i + 1) - whitePoints.get(i)));
+        for (var entry : dotsMap.entrySet()) {
+            sum += getValue(entry.getValue());
         }
-
-        for (int i = 0; i < blackPoints.size(); i++) {
-            if (i == 0) {
-                sum += blackPoints.get(1) - blackPoints.get(0);
-                continue;
-            }
-            if (i == blackPoints.size() - 1) {
-                sum += blackPoints.get(blackPoints.size() - 1) - blackPoints.get(blackPoints.size() - 2);
-                continue;
-            }
-            sum += Math.min(Math.abs(blackPoints.get(i - 1) - blackPoints.get(i)), Math.abs(blackPoints.get(i + 1) - blackPoints.get(i)));
-        }
-
         System.out.println(sum);
+    }
+
+    static int getValue(List<Integer> dots) {
+        Collections.sort(dots);
+        int sum = 0;
+        for (int i = 0; i < dots.size(); i++) {
+            if (i == 0) {
+                sum += dots.get(i + 1) - dots.get(i);
+                continue;
+            }
+            if (i == dots.size() - 1) {
+                sum += dots.get(i) - dots.get(i - 1);
+                continue;
+            }
+            sum += Math.min(Math.abs(dots.get(i) - dots.get(i - 1)), Math.abs(dots.get(i) - dots.get(i + 1)));
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
