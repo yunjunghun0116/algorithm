bab221bbc24d3e7997ba0c0b1625954f9f922918
윤정훈 <76200940+yunjunghun0116@users.noreply.github.com>
2025-02-02 15:58:56 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 184 ms, Memory: 17872 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,57 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-	static int max_value = -1000000000;
-	static int min_value = 1000000000;
-	static int count = 0;
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int cnt = sc.nextInt();
-		int[] arr = new int[cnt];//숫자
-		int[] arith = new int[4];//사칙연산 + - * /계산은 앞에서부터		
-		//시작은 arr[0]부터, arr[1]부터 계산
-		for(int i = 0; i < cnt; i++) {
-			arr[i] = sc.nextInt();
-		}
-		for(int i = 0; i < 4; i++) {
-			arith[i] = sc.nextInt();//2 1 1 1
-			count=count+arith[i];
-		}
+    public static int n;
+    public static List<Integer> numberList = new ArrayList<>();
+    public static int[] opArray = new int[4];
+    public static int maxValue = Integer.MIN_VALUE;
+    public static int minValue = Integer.MAX_VALUE;
 
-		inArith(arr,1,arr[0],arith[0],arith[1],arith[2],arith[3]);
-		
-		System.out.println(max_value);	
-		System.out.println(min_value);
-		
-	}
-	
-	public static void inArith(int[] a,int index,int sum,int plus,int minus,int multi,int divide) {
-		if(index > count) {//count == a의 마지막index와 동일
-			min_value = Math.min(sum, min_value);
-			max_value = Math.max(sum, max_value);
-			return;
-		}
-		if(plus>0) {
-			inArith(a,index+1,sum+a[index],plus-1,minus,multi,divide);
-		}
-		if(minus>0) {
-			inArith(a,index+1,sum-a[index],plus,minus-1,multi,divide);
-		}
-		if(multi>0) {
-			inArith(a,index+1,sum*a[index],plus,minus,multi-1,divide);
-		}
-		if(divide>0) {
-			int newsum;
-			if(sum >= 0) {
-				newsum = sum / a[index];
-			}else {
-				newsum = (-1)*((sum*(-1))/a[index]);
-			}
-			inArith(a,index+1,newsum,plus,minus,multi,divide-1);
-		}
-		
-	}
+    public static void main(String[] args) {
+        initialize();
+        solve(1, numberList.get(0));
+        System.out.println(maxValue);
+        System.out.println(minValue);
+    }
 
+    public static void initialize() {
+        Scanner sc = new Scanner(System.in);
+        n = Integer.parseInt(sc.nextLine());
+
+        for (String number : sc.nextLine().split(" ")) {
+            numberList.add(Integer.parseInt(number));
+        }
+
+        String[] opInput = sc.nextLine().split(" ");
+        for (int i = 0; i < 4; i++) {
+            opArray[i] = Integer.parseInt(opInput[i]);
+        }
+    }
+
+    public static void solve(int currIndex, int currNumber) {
+        if (currIndex == n) {
+            maxValue = Math.max(maxValue, currNumber);
+            minValue = Math.min(minValue, currNumber);
+            return;
+        }
+        for (int i = 0; i < 4; i++) {
+            if (opArray[i] == 0) continue;
+            opArray[i] = opArray[i] - 1;
+            solve(currIndex + 1, operation(i, currNumber, numberList.get(currIndex)));
+            opArray[i] = opArray[i] + 1;
+        }
+    }
+
+    public static int operation(int opIndex, int originNumber, int nextNumber) {
+        switch (opIndex) {
+            case 0: {
+                return originNumber + nextNumber;
+            }
+            case 1: {
+                return originNumber - nextNumber;
+            }
+            case 2: {
+                return originNumber * nextNumber;
+            }
+            default: {
+                if (originNumber < 0 && nextNumber > 0) {
+                    return -1 * ((-1 * originNumber) / nextNumber);
+                }
+                return originNumber / nextNumber;
+            }
+        }
+    }
 }
